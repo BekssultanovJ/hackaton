41f9075ab21808bda1aaebef1212c82af1e60520
juma <jumabeksultanov125@gmail.com>
2022-01-20 17:24:24 +0600

    asd

---
+++ main/views.py
@@ -0,0 +1,91 @@
+from .serializers import *
+from .models import *
+from rest_framework import status, viewsets, mixins
+from .filters import ProductFilter, OrderFilter
+from rest_framework.filters import OrderingFilter
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import IsAuthorOrAdminPermission, DenyAll
+import django_filters.rest_framework as filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = ProductFilter
+    ordering_fields = ['title', 'price']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductListSerializer
+        return self.serializer_class
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        elif self.action in ['create_review', 'like']:
+            return [IsAuthenticated()]
+        return []
+
+    @action(detail=True, methods=['POST'])
+    def create_review(self, request, pk):
+        data = request.data.copy()
+        data['product'] = pk
+        serializer = ReviewSerializers(data=data, context={'request': request})
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
+
+    @action(detail=True, methods=['POST'])
+    def like(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        like_obj, created = WishList.objects.get_or_create(product=product, user=user)
+        if like_obj.is_like:
+            like_obj.is_like = False
+            like_obj.save()
+            return Response('disliked')
+        else:
+            like_obj.is_like = True
+            like_obj.save()
+            return Response('liked')
+
+
+class ReviewViewSet(mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializers
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAuthenticated()]
+        return [IsAuthorOrAdminPermission()]
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter)
+    filterset_class = OrderFilter
+    ordering_fields = ['total_sum', 'created_at']
+
+    def get_permissions(self):
+        if self.action in ['create', 'list', 'retrieve']:
+            return [IsAuthenticated()]
+        elif self.action in ['update', 'partial_update']:
+            return [IsAdminUser()]
+        else:
+            return [DenyAll()]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+        return queryset
